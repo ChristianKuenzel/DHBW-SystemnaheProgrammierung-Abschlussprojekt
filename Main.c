9a917b3e431284729150728a7054acd3aa5cec90
Christian Künzel <73838333+ChristianKuenzel@users.noreply.github.com>
2021-12-05 15:01:49 +0100

    Update Main.c
    Added commentary

--- Main.c
+++ Main.c
@@ -1,29 +1,45 @@
 /*
- * File:   Main.c
- * Author: Leonard Kipar & Christian Martin K�nzel
- *
- * Created on 1. Dezember 2021, 9:31
- */
+Copyright 2021, DHBW Lörrach
+Systemnahe Pogrammierung, Abschlussprojekt Piano
 
+Group #8:
+Leonard Kipar <Mail> <https://github.com/leonard-kipar>
+Christian Künzel <chriskuenzel@web.de> <https://github.com/ChristianKuenzel>
 
+GitHub: <https://github.com/ChristianKuenzel/DHBW-SystemnaheProgrammierung-Abschlussprojekt>
+
+MPLab & Prometheus
+Microcontroller: PIC18F4520
+*/
+
+// Includes.
 #include "Definitions.h"
 
+// Functions.
+// Calculate the time (ms) necessary to create a specific tone by formula.
 int calculateTime(int note) {
+    // Calculate time. Multiply by 1 million to create correct amount of ms.
     float n;
     n = ((1/note)/2)*1000000;
+    
+    // Round down the number by converting into integer and cutting of the rest.
     int x;
     x = (int)n;
     return x;
 }
 
+// Delay a certain amount of time.
+// Needed because __delay_ms(x) only works with static numbers.
 void delay_ms(int time) {
     for(int j=0; j<time; j++) {
         __delay_ms(1);
     }
 }
 
+// Create a given tone by activating the output to the speaker for a given amount of time.
 void playTone(int note)
 {
+  // Calculate the time needed to create the tone.
   int time = calculateTime(note);
   for(int i=0; i<note; i++)
     {
@@ -34,15 +50,22 @@ void playTone(int note)
     }
 }
 
+// Main function.
 void main(void) {
-    //Constructor
+    // Set APins, BPins and CPin.
+    // Inputs.
     TRISA = 0b11111111;
-    TRISB = 0b11111111;  
-    TRISCbits.RC0 = 0; //RC0 = Output
+    TRISB = 0b11111111;
     
+    // Ouput
+    TRISCbits.RC0 = 0;
+    
+    // Start loop, waiting for button/user input.
     while(1) {
         Nop();
         Nop();
+        
+        // Check if pin got activated. Then play tone.
         if(pinInput1 == 0){
             playTone(c);
         }
